f817c94314366ccca3720ba2099bd7e3acf32ac7
Tsvoaa <gugudndn@naver.com>
2022-12-29 20:11:39 +0900

    초기 커밋.

---
+++ OcrTranslation/Form1.cs
@@ -0,0 +1,10 @@
+namespace OcrTranslation
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ OcrTranslation/Papago.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OcrTranslation
+{
+    internal class Papago
+    {
+        public void Translation(int txtLen)
+        {
+            // Papago API 사용을 위한 객체 생성
+            WebRequest webRequest = null;
+            WebResponse webResponse = null;
+            Stream stream = null;
+            StreamReader streamReader = null;
+
+            // Papago API url
+            string url = "https://openapi.naver.com/v1/papago/n2mt";
+
+
+
+        }
+
+    }
+}
