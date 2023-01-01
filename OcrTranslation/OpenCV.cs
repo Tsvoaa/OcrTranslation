c2b3892c4ee724c0dfbf18abf50b1617935e28fd
Tsvoaa <gugudndn@naver.com>
2023-01-02 00:40:03 +0900

    OpenCVSharp4 좌표 압축 중간 단계

--- OcrTranslation/OpenCV.cs
+++ OcrTranslation/OpenCV.cs
@@ -320,9 +320,9 @@ namespace OcrTranslation
                 int baseY = (int)(minY + maxY) / 2;
                 int baseX = (int)(minX + maxX) / 2;
 
-                if(ySort[i, 1] + 3 > baseY || ySort[i, 1] - 3 < baseY)
+                if(ySort[i, 1] + 3 > maxY || ySort[i, 1] - 3 < minY)
                 {
-                    if(ySort[i, 0] + 100 > baseX || ySort[i, 0] - 100 < baseX)
+                    if(ySort[i, 0] + 50 > maxX || ySort[i, 0] - 50 < minX)
                     {
                         if (minY > ySort[i, 1])
                         {
@@ -374,33 +374,131 @@ namespace OcrTranslation
                     rectCount++;
                 }
 
-                rectLocation[rectCount, 0] = minX;
-                rectLocation[rectCount, 1] = minY;
-                rectLocation[rectCount, 2] = maxX;
-                rectLocation[rectCount, 3] = maxY;
-
-                minX = ySort[rectCount, 0];
-                minY = ySort[rectCount, 1];
-                maxX = ySort[rectCount, 0];
-                maxY = ySort[rectCount, 1];
-
-                rectCount++;
+                
 
             }
 
+            // 좌표값을 압축시켜주는 코드
+            int width = 0;
+            int height = 0;
+
+            width = rectLocation[0,2] - rectLocation[0, 0];
+            height = rectLocation[0,3] - rectLocation[0, 1];
+
+            int[,] rectCompess = new int[1000, 4];
+            int[,] rectCompess2;
+            int rectCompessCount = 0;
+
+            rectCompess2 = rectLocation;
+
+            for(int j = 0; j < 5; j++)
+            {
+                
+
+                for (int i = 1; i < rectCompess2.GetLength(0); i += 2)
+                {
+                    if (rectCompess2[i, 0] == 0 && rectCompess2[i, 1] == 0 && rectCompess2[i, 2] == 0 && rectCompess2[i, 3] == 0)
+                    {
+                        break;
+                    }
+
+                    if (rectCompess2[i - 1, 0] + 100 > rectCompess2[i, 0] && rectCompess2[i - 1, 0] < rectCompess2[i, 0])
+                    {
+                        rectCompess[rectCompessCount, 0] = rectCompess2[i - 1, 0];
+                    }
+                    else
+                    {
+                        rectCompess[rectCompessCount, 0] = rectCompess2[i, 0];
+                    }
+
+                    if (rectCompess2[i - 1, 1] + 100 > rectCompess2[i, 1] && rectCompess2[i - 1, 1] < rectCompess2[i, 1])
+                    {
+                        rectCompess[rectCompessCount, 1] = rectCompess2[i - 1, 1];
+                    }
+                    else
+                    {
+                        rectCompess[rectCompessCount, 1] = rectCompess2[i, 1];
+                    }
+
+                    if (rectCompess2[i - 1, 2] < rectCompess2[i, 2] + 100 && rectCompess2[i - 1, 2] > rectCompess2[i, 2])
+                    {
+                        rectCompess[rectCompessCount, 2] = rectCompess2[i - 1, 2];
+                    }
+                    else
+                    {
+                        rectCompess[rectCompessCount, 2] = rectCompess2[i, 2];
+                    }
+
+                    if (rectCompess2[i - 1, 3] < rectCompess2[i, 3] + 100 && rectCompess2[i - 1, 3] > rectCompess2[i, 3])
+                    {
+                        rectCompess[rectCompessCount, 3] = rectCompess2[i - 1, 3];
+                    }
+                    else
+                    {
+                        rectCompess[rectCompessCount, 3] = rectCompess2[i, 3];
+                    }
+
+                    rectCompessCount++;
+                }
+
+                rectCompess2 = rectCompess;
+                rectCompess = new int[1000, 4];
+                rectCompessCount = 0;
+
+            }
+
+
+            rectCompess = rectCompess2;
+
             
 
-            for(int i = 0; i < rectLocation.GetLength(0); i++)
+           
+
+           
+
+            
+
+
+            for (int i = 0; i < rectCompess.GetLength(0); i++)
+            {
+                Debug.WriteLine(rectCompess[i, 0] + "  " + rectCompess[i, 1] + "  " + rectCompess[i, 2] + "  " + rectCompess[i, 3]);
+            }
+
+            
+            // 좌표값으로 사각형을 그려주는 코드
+            for(int i = 0; i < rectCompess.GetLength(0); i++)
             {
                 //Debug.WriteLine(rectLocation[i, 0] + "  " + rectLocation[i, 1] + "  " + rectLocation[i, 2] + "  " + rectLocation[i, 3]);
 
-                
-
-                if (!(rectLocation[i, 0] == 0 && rectLocation[i, 1] == 0 && rectLocation[i, 2] == 0 && rectLocation[i, 3] == 0))
+                if (!(rectCompess[i, 0] == 0 && rectCompess[i, 1] == 0 && rectCompess[i, 2] == 0 && rectCompess[i, 3] == 0))
                 {
-                    Rect locationRect = new Rect(rectLocation[i, 0], rectLocation[i, 1], rectLocation[i, 2], rectLocation[i, 3]);
-                    Cv2.Rectangle(result, locationRect, Scalar.Blue, 2);
+                    
+
+                    if (rectCompess[i, 2] - rectCompess[i, 0] > 2000 || rectCompess[i, 3] - rectCompess[i, 1] > 2000)
+                    {
+                        
+                    }
+                    else
+                    {
+                        if (rectCompess[1, 2] - rectCompess[i, 0] > 0 || rectCompess[i, 3] - rectCompess[i, 1] > 0)
+                        {
+                            
+                        }
+                        else
+                        {
+                            
+                        }
+                        
+
+                    }
                 }
+                else
+                {
+                    break;
+                }
+
+                Rect locationRect = new Rect(rectCompess[i, 0] + 10, rectCompess[i, 1] - 50, rectCompess[i, 2], rectCompess[i, 3]);
+                Cv2.Rectangle(result, locationRect, Scalar.Blue, 2);
 
             }
 
